b2b805a18bbef7787ebf747ff9b50c450c766126
Kendra Vant <kendra.vant@gmail.com>
2014-04-25 21:05:32 +1000

    Updated the readme and cleaned up the analysis file somewhat.

--- README.md
+++ README.md
@@ -3,9 +3,35 @@ README
 
 The file run_analysis.R reads in the datasets:
 
-X_test.txt
-X_train.txt
-y_text.txt
-t_train.txt
+	X_test.txt
+	X_train.txt
+	y_text.txt
+	t_train.txt
+	subject_test.txt
+	subject_train.txt
 
-and merges both the 
+and merges the test and training datasets together
+
+It also reads in the feature and activity names from: 
+
+	features.txt
+	activity_labels.txt
+
+and creates standardised variable names using the word.word.word convention
+
+Two data sets are created and written out to file
+
+	meanAndStandardDeviationFeatures.txt 
+
+		which contains only the mean and standard deviation features along with the subject ID and activity
+	
+
+	featureAveragebySubjectAndActivity.txt 
+
+		which contains the average for each feature by subject ID and activity
+
+
+Required libraries (loaded by run_analysis.R): 
+
+	reshape
+	plyr
--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,20 @@
-# Creates tidy datasets containing both test and training data for 
+# Creates two tidy datasets containing both test and training data for 
 # Samsung Galaxy S accelerometer data 
-
+# Set 1 (meanAndStandardDeviationFeatures.txt) contains only the mean and standard deviation features
+# Set 2 (featureAveragebySubjectAndActivity.txt) contains the average for each selected feature by subject and activity
 
 # Load required libraries
-library(reshape)
 library(plyr)
+library(reshape)
+library(stringr)
 
 # Read in the data
 XTest <- read.table("UCI_HAR_Dataset//test//X_test.txt", nrows = 2950, comment.char = "", colClasses = c("numeric"))
 XTrain <- read.table("UCI_HAR_Dataset//train//X_train.txt", nrows = 7355, comment.char = "", colClasses = c("numeric"))
 yTest <- read.table("UCI_HAR_Dataset//test//y_test.txt", colClasses = c("factor"))
 yTrain <- read.table("UCI_HAR_Dataset//train//y_train.txt", colClasses = c("factor"))
-subjectTest <- read.table("UCI_HAR_Dataset//test//subject_test.txt", colClasses = c("factor"))
-subjectTrain <- read.table("UCI_HAR_Dataset//train//subject_train.txt", colClasses = c("factor"))
+subjectTest <- read.table("UCI_HAR_Dataset//test//subject_test.txt")
+subjectTrain <- read.table("UCI_HAR_Dataset//train//subject_train.txt")
 
 # Read in the feature and activity names
 features <- read.table("UCI_HAR_Dataset//features.txt")
@@ -21,46 +23,45 @@ names(features) <- c("colNumber","featureName")
 names(activities) <- c("activityID","activityName")
 
 # Combine the training and test data and remove intermediate variables
-X <- rbind(XTest,XTrain)
-y <- rbind(yTest,yTrain) 
+X <- rbind(XTrain,XTest)
+y <- rbind(yTrain,yTest) 
 y <- revalue(y$V1, c("1"=activities$activityName[1], 
                      "2"=activities$activityName[2],
                      "3"=activities$activityName[3],
                      "4"=activities$activityName[4],
                      "5"=activities$activityName[5],
                      "6"=activities$activityName[6]))
-subject <- rbind(subjectTest,subjectTrain)
+subject <- rbind(subjectTrain,subjectTest)
 rm(subjectTest, subjectTrain, yTest, yTrain, XTest, XTrain)
 
-
 # Select only the features which are either a mean or a standard deviation
-featuresWithMean <- grep("mean",features$featureName)
-featuresWithStd <- grep("std",features$featureName)
+# Including meanFreq variables here because they feel like means
+featuresWithMean <- grep("mean",features$featureName,fixed=TRUE)
+featuresWithStd <- grep("std()",features$featureName,fixed=TRUE)
 featuresToKeep <- sort(c(featuresWithMean,featuresWithStd))
-XSelectedFeatures <- X[,featuresToKeep]
 
+# Create a tidy names vector
+namesVector <- c("subject.id","activity", as.character(features$featureName[featuresToKeep]))
+namesVector <- gsub("([a-z])([A-Z])", "\\1.\\2", namesVector, perl = TRUE)
+namesVector <- gsub("-",".", namesVector, perl=TRUE)
+namesVector <- gsub(",",".", namesVector, perl=TRUE)
+namesVector <- gsub("()","", namesVector, fixed=TRUE)
+namesVector <- tolower(namesVector)
 
-# Create a dataset with the subject ID, the activity and the mean and standard deviation features
-# and write this out to the file meanAndStandardDeviationActivities.txt
-meanAndStandardDeviationFeatures <-  cbind(subject,y,XSelectedFeatures)
-names(meanAndStandardDeviationFeatures) <- c("subjectID","activity", as.character(features$featureName[featuresToKeep]))
-write.table(meanAndStandardDeviationFeatures,"meanAndStandardDeviationFeatures.txt",row.names=FALSE)
+# Create a dataset with the subject ID, the activity and the mean and standard deviation features 
+# and write it out to the file meanAndStandardDeviationFeatures.csv
+meanAndStandardDeviationFeatures <-  cbind(subject,y,X[,featuresToKeep])
+names(meanAndStandardDeviationFeatures) <- namesVector
+meanAndStandardDeviationFeatures <- arrange(meanAndStandardDeviationFeatures, 
+                                            meanAndStandardDeviationFeatures$subject.id)
+write.csv(meanAndStandardDeviationFeatures,"meanAndStandardDeviationFeatures.csv",row.names=FALSE)
 
 
 # Create a dataset of the average for each feature by activity and by subject
-allFeatures = cbind(subject,y,X)
-names(allFeatures) <- c("subjectID","activity", as.character(features$featureName))
-meltedData <- melt(allFeatures, id = c("subjectID","activity"))
-averagesTable <-cast(meltedData, subjectID + activity ~ variable, mean)
-averagesTable$subjectID <- as.numeric(averagesTable$subjectID)
-averagesTable <- arrange(averagesTable, averagesTable$subjectID)
-  
-# Update the names in the averages table
-newNames <- paste("average(",names(averagesTable),")",sep="")
-newNames[1] <- c("subjectID")
-newNames[2] <- c("activity")
-names(averagesTable) <- newNames
+# and write it out to the file featureAveragebySubjectAndActivity.csv
+meltedData <- melt(meanAndStandardDeviationFeatures, id = c("subject.id","activity"))
+averagesTable <-cast(meltedData, subject.id + activity ~ variable, mean)
+write.csv(averagesTable,"featureAveragebySubjectAndActivity.csv",row.names=FALSE)
 
-# Write out the dataset with activity averages per subject per activity 
-# to the file featureAveragebySubjectAndActivity.txt
-write.table(averagesTable,"featureAveragebySubjectAndActivity.txt",row.names=FALSE)
+
+
